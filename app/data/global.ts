1d30f13712de7ce0a3f09eae1c6088ecbd46c816
Derek <rumbleincybertron@gmail.com>
2023-07-28 20:10:49 +0900

    + global data init

---
+++ app/data/global.ts
@@ -0,0 +1,19 @@
+type Route = {
+  title: string;
+  path: string;
+}
+
+export const routes: Route[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Designs",
+    path: "/designs",
+  },
+];

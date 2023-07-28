2ebd242f6e5ee5c75ae08edad4e7cf1b29bb858e
Derek <rumbleincybertron@gmail.com>
2023-07-28 20:07:59 +0900

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+  reactStrictMode: true,
+  output: 'standalone',
+}
 
 module.exports = nextConfig

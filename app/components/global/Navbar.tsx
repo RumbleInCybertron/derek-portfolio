922da7c504a24685cc51b17219cb676e8be06f36
Derek <rumbleincybertron@gmail.com>
2023-07-29 20:22:54 +0900

    ch navbar cmpnt

--- app/components/global/Navbar.tsx
+++ app/components/global/Navbar.tsx
@@ -1,89 +1,47 @@
-'use client';
-
-import { FiSun, FiMoon } from "react-icons/fi";
 import Link from "next/link";
+import React from "react";
 import Image from "next/image";
-import useDarkMode from "@/utils/hooks/useDarkMode";
+import { routes } from "@/app/data/global"
 
-const Appbar = ({ currentPage }) => {
-
-  const [colorTheme, setTheme] = useDarkMode();
-
+function Appbar({ currentPage }) {
   return (
-    <>
-      <nav>
-        <li>
-          <Link href="/">
-            <span>
-              <Image
-                className="mr-2 transform hover:rotate-360 hover:scale-75 transition-transform duration-500"
-                src="/static/logos/KitsuneCom.jpg"
-                alt=""
-                width="60"
-                height="60"
-              />
-              {"D-Roc".split("").map((letter, index) => {
-                return(
-                  <span key={index}></span>
-                );
-              })}
-            </span>
-          </Link>
-        </li>
-        {/* <header className="flex justify-between container items-center py-2 px-4 shadow-sm">
-          <div className="flex space-x-5">
-            <Link href="/">
-              <span className="text-lg font-bold">Logo</span>
-            </Link>
-            <Link href="/">
-              <span className="text-gray-500 hover:text-gray-900">Home</span>
-            </Link>
-            <Link href={"/"}>
-              <span className="text-gray-500 hover:text-gray-900">Home</span>
-            </Link>
-            <Link href="/">
-              <span className="text-gray-500 hover:text-gray-900">Home</span>
-            </Link>
-          </div>
-
-          <div className="flex items-center space-x-4">
-            {colorTheme === "light" ? (
-              <button
-                type="button"
-                aria-label="Toggle dark mode"
-                onClick={() => setTheme("light")}
-              >
-                {<FiSun />}
-              </button>
-            ) : (
-              <button
-                type="button"
-                aria-label="Toggle dark mode"
-                onClick={() => setTheme("dark")}
-              >
-                {<FiMoon />}
-              </button>
-
-            )}
-
-            <div className="m-2">
-              <Link href="/">
-                <span className="m-1 bg-purple-600 hover:bg-green-600 text-white px-4 py-2 rounded-full">
-                  <a>Home</a>
+    <nav className="flex items-center justify-between">
+      <li className="list-none font-bold text-lg cursor-pointer">
+        <Link href="/">
+          <span className="font-black text-xl flex items-center">
+            <Image
+              className="mr-2 transform hover:rotate-360 hover:scale-75 transition-transform duration-500"
+              src="/static/logos/KitsuneCom.jpg"
+              alt=""
+              width="60"
+              height="60"
+            />
+            {"D-Roc".split("").map((letter, index) => {
+              return (
+                <span key={index} className="hover:text-fun-pink hover:mt-2 transition-all duration-500 hover:duration-100">
+                  {letter}
                 </span>
-              </Link>
-              <Link href="/">
-                <span className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full">
-                  <a>Home</a>
-                </span>
-              </Link>
-            </div>
-          </div>
-        </header> */}
-      </nav >
-    </>
-
-
+              );
+            })}
+          </span>
+        </Link>
+      </li>
+      <ul className="flex items-center space-x-10">
+        {routes.map((item, index) => {
+          return (
+            <li
+              key={index}
+              className={`list-none text-white ${currentPage === item.title
+                  ? "opacity-100"
+                  : "opacity-40 hover:opacity-100 transition-opacity"
+                }`}
+            >
+              <Link href={item.path}>{item.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   );
 };
 

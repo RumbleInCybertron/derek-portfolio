2103f21748a657747c3927f76b32996137215599
Derek <rumbleincybertron@gmail.com>
2023-07-31 22:42:08 +0900

    update hero

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,8 +1,14 @@
-function Hero(){
-  return(
+function Hero() {
+  return (
     <>
-      <div className="relative">
-        <p className="">Hey, I am D-Roc.</p>
+      <div
+        className="z-1 relative w-full pt-20 pb-40 m-auto flex flex-col items-center justify-center text-center heroElem"
+        style={{ maxWidth: "1200px" }}
+      >
+        <p className="text-xl mb-5">Hey, This is D-Roc.</p>
+        <h1 className="relative inline-block font-bold text-5xl max-w-2xl lg:max-w-4xl lg:text-7xl md:text-6xl tracking-tighter mb-10 w-auto heroTitle">
+          <span className="text-fun-pink">Innovating</span> and <span className="text-fun-pink">designing</span> for the <span className="text-fun-pink">web</span> and for <span className="text-fun-pink">your business</span>.
+        </h1>
       </div>
     </>
   )

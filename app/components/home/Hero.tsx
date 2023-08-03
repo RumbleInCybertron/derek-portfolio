40aee613eacdb510363f66580c91c1676407e622
Derek <rumbleincybertron@gmail.com>
2023-08-03 19:49:02 +0900

    play w/ hero anims

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Link as ScrollLink } from "react-scroll";
+import Image from "next/image";
 
 function Hero() {
   return (
@@ -12,6 +13,76 @@ function Hero() {
         <p className="text-xl mb-5">Hey, This is D-Roc.</p>
         <h1 className="relative inline-block font-bold text-5xl max-w-2xl lg:max-w-4xl lg:text-7xl md:text-6xl tracking-tighter mb-10 w-auto heroTitle">
           <span className="text-fun-pink">Innovating</span> and <span className="text-fun-pink">designing</span> for the <span className="text-fun-pink">web</span> and for <span className="text-fun-pink">your business</span>.
+          <Image
+            className="sqD squiggle-hero-html w-16 top-[-90px] right-[5%] sm:top-[-90px] sm:right-[170px]"
+            style={{ animationDelay: "0.1s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD squiggle-hero-nextjs hidden top-[75px] right-0 w-11"
+            style={{ animationDelay: "0.2s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD bottom-[-300px] -right-1/4 sm:right-[-20%] lg:bottom-[-310px] lg:right-[-120px] w-[400px]"
+            style={{ animationDelay: "0.3s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD hidden sm:block bottom-[-340px] left-[-180px]"
+            style={{ animationDelay: "0.4s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD hidden sm:block left-[100px] lg:left-[160px] bottom-[-150px]"
+            style={{ animationDelay: "0.5s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD bottom-[-320px] right-[65%] sm:right-[45%]"
+            style={{ animationDelay: "0.6s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD right-[-60px] sm:right-0 bottom-[-180px] lg:[5%]"
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD squiggle-hero-pop1 hidden sm:block sm:top-[-130px] sm:left-[15%] lg:top-[-130px] lg:left-[120px]"
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
+          <Image
+            className="sqD left-[-35px] bottom-[-85px] sm:bottom-[-100px] sm:left-5 opacity-40"
+            style={{ animationDelay: "0.9s" }}
+            src="/static/icons/external-link.svg"
+            alt=""
+            width="1000"
+            height="1000"
+          />
         </h1>
         <ScrollLink
           activeClass="active"

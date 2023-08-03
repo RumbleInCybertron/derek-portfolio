e10f3a78dfe6ba244bb59631f489922f8929324a
Derek <rumbleincybertron@gmail.com>
2023-08-03 17:12:01 +0900

    format/design section title txt & + horiz ln

--- app/components/global/SectionTitle.tsx
+++ app/components/global/SectionTitle.tsx
@@ -4,8 +4,8 @@ type SectionTitleProps = {
 
 function SectionTitle({ title }: SectionTitleProps) {
   return (
-    <div>
-      <h2>
+    <div className="w-full pt-5 overflow-x-hidden">
+      <h2 className="landingSectionTitle max-w-sm md:max-w-max mx-auto md:mx-0 text-center md:text-left relative text-3xl font-bold mb-10 pt-4 md:pt-0 md:w-max">
         {title}
       </h2>
     </div>

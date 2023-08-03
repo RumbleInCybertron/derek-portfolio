c23f5cc4f3c9ecfc7ae9a7b67845a767f6fe1217
Derek <rumbleincybertron@gmail.com>
2023-08-03 15:54:14 +0900

    + projects content mock init

---
+++ app/data/content/projects.ts
@@ -0,0 +1,44 @@
+import { kebabCase } from "@/app/utils/utils";
+import { Project } from "@/app/types/page";
+
+const projects: Project[] = [
+  {
+    id: 0,
+    title: "Project 1",
+    desc: "Sample Description for Project 1",
+    img: "/static/projects/skrizzle.jpg",
+    link: "https://google.com/",
+    github: "https://github.com/RumbleInCybertron/Skrizzle",
+    tags: ["Swift"],
+  },
+  {
+    id: 1,
+    title: "Project 2",
+    desc: "Sample Description for Project 2",
+    img: "/static/projects/skrizzle.jpg",
+    link: "https://google.com/",
+    github: "https://github.com/RumbleInCybertron/Skrizzle",
+    tags: ["Swift"],
+  },
+  {
+    id: 2,
+    title: "Project 3",
+    desc: "Sample Description for Project 3",
+    img: "/static/projects/skrizzle.jpg",
+    link: "https://google.com/",
+    github: "https://github.com/RumbleInCybertron/Skrizzle",
+    tags: ["Swift"],
+  },
+];
+
+export const allTags = [];
+
+projects.forEach((project) => {
+  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag))
+});
+
+export const allKebabTags = allTags.map(tag => (
+  kebabCase(tag)
+));
+
+export default projects;

10d32b76afd8ab85ce7d9aa6cbe92f9b049cd0ce
Derek <rumbleincybertron@gmail.com>
2023-08-03 19:48:38 +0900

    refactor

--- app/components/utility/Page.tsx
+++ app/components/utility/Page.tsx
@@ -20,47 +20,48 @@ export default function Page({ currentPage, meta: { title, desc }, children }: P
     : `${currentPage} - Derek.Haus`
     }`;
   console.log("Current Page: ", currentPage);
-  return (<>
-    <Script
-      id="Google Analytics"
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  return (
+    <>
+      <Script
+        id="Google Analytics"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
-      }}
-    />
-    <div
-      className="w-full m-auto flex flex-col items-center justify-center min-h-screen text-white overflow-hidden md:overflow-visible"
-      style={{ maxWidth: "1200px" }}
-    >
-      <Head>
-        <title>{pageTitle}</title>
+        }}
+      />
+      <div
+        className="w-full m-auto flex flex-col items-center justify-center min-h-screen text-white overflow-hidden md:overflow-visible"
+        style={{ maxWidth: "1400px" }}
+      >
+        <Head>
+          <title>{pageTitle}</title>
 
-        <Link href="/favicon.ico" />
-        <Link href="/favicon.ico" />
-        <Link href="/favicon.ico" />
-        <Link href="/static/favicon/site.webmanifest" />
+          <Link href="/favicon.ico" />
+          <Link href="/favicon.ico" />
+          <Link href="/favicon.ico" />
+          <Link href="/static/favicon/site.webmanifest" />
 
-        <meta name="title" content={pageTitle} />
-        <meta name="description" content={desc} />
+          <meta name="title" content={pageTitle} />
+          <meta name="description" content={desc} />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://derek.haus/" />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={desc} />
-        <meta
-          property="og:image"
-          content="https://derek.haus/static/misc/og.png" // TODO: design/create/add og.png
-        />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://derek.haus/" />
+          <meta property="og:title" content={pageTitle} />
+          <meta property="og:description" content={desc} />
+          <meta
+            property="og:image"
+            content="https://derek.haus/static/misc/og.png" // TODO: design/create/add og.png
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -68,19 +69,19 @@ export default function Page({ currentPage, meta: { title, desc }, children }: P
                 page_path: window.location.pathname,
               });
           `,
-          }}
-        />
-      </Head>
-      <main className="flex-1 p-5 w-full text-center">
-        <div className="hidden sm:block z-100">
-          <Navbar currentPage={currentPage} />
-        </div>
-        <div className="-m-5 block sm:hidden z-100">
-          <MobileNavbar />
-        </div>
-        {children}
-      </main>
-    </div>
-  </>
+            }}
+          />
+        </Head>
+        <main className="flex-1 p-5 w-full text-center">
+          <div className="hidden sm:block z-100">
+            <Navbar currentPage={currentPage} />
+          </div>
+          <div className="-m-5 block sm:hidden z-100">
+            <MobileNavbar />
+          </div>
+          {children}
+        </main>
+      </div>
+    </>
   )
 };

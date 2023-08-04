af5026b1e2dd9ccd357baa1469c77e97c8a18614
Derek <rumbleincybertron@gmail.com>
2023-08-05 01:07:19 +0900

    + testimonials css

--- app/components/home/Testimonials.tsx
+++ app/components/home/Testimonials.tsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 
 function Testimonials() {
   return (
-    <div>
+    <div className="flex flex-col text-left max-w-md md:max-w-full m-auto">
       <SectionTitle title="What's the skuttlebutt?" />
-      <div>
+      <div className="max-w-5xl m-auto md:max-h-[200] grid grid-cols-1 md:grid-cols-3 gap-10 items-start">
         {testimonials.map((item, index) => {
           return (
             <div
             key={index}
-            className=""
+            className="flex flex-col justify-between relative bg-fun-pink-darker border border-fun-pink-light p-5 rounded-lg h-full"
             >
               <p className="text-base italic relative testimonialQuote">
                "{item.quote}"
               </p>
-              <p>
-                <b>{item.name}</b>
+              <p className="mt-4 text-xs text-fun-gray">
+                <b className="text-fun-pink font-monospace">{item.name}</b> -{" "}
                 {item.job}
               </p>
             </div>

ae865b44755935d6cd034e7399de17d96711ab23
Derek <rumbleincybertron@gmail.com>
2023-08-07 20:33:04 +0900

    + projects more cmpnt

---
+++ app/components/projects/More.tsx
@@ -0,0 +1,9 @@
+function More() {
+  return (
+    <p className="py-20 text-sm italic text-fun-gray">
+      Hey you! Yeah, you. Check it out. I have got even more on <a className="text-fun-pink underline" href="https://github.com/RumbleInCybertron" target="_blank">my GitHub</a>!
+    </p>
+  );
+}
+
+export default More

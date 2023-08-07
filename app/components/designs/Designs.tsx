c222fd26d5508d8437ba122dc5af813ca204149f
Derek <rumbleincybertron@gmail.com>
2023-08-08 02:28:23 +0900

    + designs cmpnt init

---
+++ app/components/designs/Designs.tsx
@@ -0,0 +1,29 @@
+import { designs } from "@/app/data/content/designs";
+import Image from "next/image";
+
+function Designs() {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 items-start">
+      {designs.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="w-full"
+          >
+            <a href={item.link} target="_blank" className="w-full">
+              <Image
+                className="w-full h-96 hover:opacity-75 transition-opacity object-cover"
+                src={item.img}
+                alt=""
+                width={48}
+                height={48}
+              />
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  )
+};
+
+export default Designs;

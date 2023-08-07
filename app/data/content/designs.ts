5b79e3e4f2a29166a8be5ad9e3035f0fc4c4c144
Derek <rumbleincybertron@gmail.com>
2023-08-08 02:28:57 +0900

    + designs data mock init

---
+++ app/data/content/designs.ts
@@ -0,0 +1,48 @@
+type Design ={
+  label: string;
+  img: string;
+  link: string;
+}
+
+export const designs: Design[] = [
+  {
+    label: "Mobile Shopping App",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "Modern App Design",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "Landing Pages for Startups - Concept",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "Luxury, Blue Portfolio",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "Mobile Deal Page",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "Neon Signatures",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "The Dentista - Logo Design",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+  {
+    label: "iPhone Wallpapers",
+    img: "/projects/skrizzle.png",
+    link: "https://google.com",
+  },
+];

8f70ef4c0c938e2db186786283c157d660286538
Derek <rumbleincybertron@gmail.com>
2023-08-17 16:37:11 +0900

    ch hero name

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
         className="z-1 relative w-full pt-20 pb-40 m-auto flex flex-col items-center justify-center text-center heroElem"
         style={{ maxWidth: "1200px" }}
       >
-        <p className="text-xl mb-5">Hey, This is D-Roc.</p>
+        <p className="text-xl mb-5">Hey, This is Derek.</p>
         <h1 className="relative inline-block font-bold text-5xl max-w-2xl lg:max-w-4xl lg:text-7xl md:text-6xl tracking-tighter mb-10 w-auto heroTitle">
           <span className="text-fun-pink">Innovating</span> and <span className="text-fun-pink">designing</span> for the <span className="text-fun-pink">web</span> and for <span className="text-fun-pink">your business</span>.
           <Image

7efcf7eaddfb961557734450d7a7aa3ebadabc89
Derek <RumbleInCybertron@users.noreply.github.com>
2023-08-18 23:19:45 +0900

    fix apos in string

--- app/components/home/Projects.tsx
+++ app/components/home/Projects.tsx
@@ -7,7 +7,7 @@ function Projects() {
   return (
     <div className="flex flex-col text-left justify-between pt-8 relative">
       <div id="learnmore">
-        <SectionTitle title="Let{`'`}s see what I{`'`}ve been up to lately." />
+        <SectionTitle title="Let&apos;s see what I&apos;ve been up to lately." />
       </div>
       <div className="grid grid-cols-1 gap-12 md:gap-5 md:grid-cols-3 items-start">
         {projects.slice(0, 3).map((item) => {
--- app/components/home/Testimonials.tsx
+++ app/components/home/Testimonials.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 function Testimonials() {
   return (
     <div className="flex flex-col text-left max-w-md md:max-w-full m-auto">
-      <SectionTitle title="What{`'`}s the skuttlebutt?" />
+      <SectionTitle title="What&apos;s the skuttlebutt?" />
       <div className="max-w-5xl m-auto md:max-h-[200] grid grid-cols-1 md:grid-cols-3 gap-10 items-start">
         {testimonials.map((item, index) => {
           return (
             <div
-            key={index}
-            className="flex flex-col justify-between relative bg-fun-pink-darker border border-fun-pink-light p-5 rounded-lg h-full"
+              key={index}
+              className="flex flex-col justify-between relative bg-fun-pink-darker border border-fun-pink-light p-5 rounded-lg h-full"
             >
               <p className="text-base italic relative testimonialQuote">
-              {`"`}{item.quote}{`"`}
+                {`"`}{item.quote}{`"`}
               </p>
               <p className="mt-4 text-xs text-fun-gray">
                 <b className="text-fun-pink font-monospace">{item.name}</b> -{" "}
